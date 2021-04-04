11e6c0d96c63328fcc2edca475d67ab602e7a339
Thehx <thehdotx@gmail.com>
2021-04-04 20:46:33 +0300

    Added sequence recorder to ease BoomBots mutator in sc2 coop

--- HotkeyHelper/Program.cs
+++ HotkeyHelper/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Configuration;
 using System.Threading;
 
 namespace HotkeyHelper
@@ -10,6 +14,8 @@ namespace HotkeyHelper
         private const VirtualKeyShort SpamKey = VirtualKeyShort.MULTIPLY;
         private const long MaxDoubleClickDelay = 200; //only do the camera moving for double-f2s within a 200ms time interval
         private const long MaxTickCount = 9000; //don't count ticks over9000 since we only require that for double-f2 recognition
+        private const VirtualKeyShort RecordSeqKey = VirtualKeyShort.LCONTROL;
+
 
         static void Main()
         {
@@ -19,6 +25,13 @@ namespace HotkeyHelper
             void ButtonDown(VirtualKeyShort key) => keybd_event((byte)key, 0, 0, UIntPtr.Zero);
             void ButtonUp(VirtualKeyShort key) => keybd_event((byte)key, 0, (uint)KEYEVENTF.KEYUP, UIntPtr.Zero);
             void ButtonPress(VirtualKeyShort key) { ButtonDown(key); ButtonUp(key); }
+            Random rn = new Random();
+            void ButtonPressRnd(VirtualKeyShort key)
+            {
+                ButtonDown(key); 
+                Thread.Sleep(rn.Next(10, 50));
+                ButtonUp(key);
+            }
             void Chord(VirtualKeyShort firstKey, VirtualKeyShort secondKey) { ButtonDown(firstKey); ButtonDown(secondKey); ButtonUp(secondKey); ButtonUp(firstKey); }
             void MouseClick() => mouse_event((int)(MouseEventFlags.LEFTDOWN | MouseEventFlags.LEFTUP), 0, 0, 0, 0);
             // working thread
@@ -28,8 +41,25 @@ namespace HotkeyHelper
                 int countWithoutSleep = 0; // make it click too much, and you're frozen
                 bool currentF2IsAlreadyProcessed = false; //a button pressed stays pressed, unless specifically unpressed by the user, so make sure we only f2 once...
                 long ticksWithoutF2 = 0; // if it's double-f2, we double the F2 in order to not lose the user's camera movement intention 
-                while (!ThreadStopSignal && (!IsKeyPressed(VirtualKeyShort.CONTROL) || !IsKeyPressed(VirtualKeyShort.KEY_C))) //we stop via ^C globally, or [Enter] in console
+                bool oldState = false;
+                Random r = new Random();
+                
+
+                while (!ThreadStopSignal) //we stop via ^C globally, or [Enter] in console
                 {
+                    if (IsKeyPressed(VirtualKeyShort.SUBTRACT) && !oldState)
+                    {
+                        oldState = true;
+                    }
+
+                    if (!IsKeyPressed(VirtualKeyShort.SUBTRACT) && oldState)
+                    {
+                        var process = Process.Start(@"C:\Users\Alex\Desktop\cliptonum.exe");
+                        process?.WaitForExit();
+                        oldState = false;
+                    }
+
+
                     var isF2Pressed = IsKeyPressed(VirtualKeyShort.F2);
                     if (!isF2Pressed) currentF2IsAlreadyProcessed = false;
                     if (isF2Pressed && !currentF2IsAlreadyProcessed)
@@ -37,10 +67,7 @@ namespace HotkeyHelper
                         if (ticksWithoutF2 < MaxDoubleClickDelay)
                         {
                             ButtonPress(VirtualKeyShort.KEY_9); //pressing 9 twice to force-select it
-                            ButtonPress(VirtualKeyShort.KEY_9);
-                            ticksWithoutF2 = 0;
-                            currentF2IsAlreadyProcessed = true;
-                            continue;
+                            ButtonPress(VirtualKeyShort.KEY_9); 
                         }
                         Chord(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_9); //Make group 9 out of all army
                         ButtonPress(VirtualKeyShort.KEY_0); //Select group 0
@@ -61,6 +88,9 @@ namespace HotkeyHelper
                     if ((countWithoutSleep = (countWithoutSleep + 1) & 7) == 7) Thread.Sleep(0);
                     MouseClick();
                 }
+
+                Console.WriteLine("Ended loop");
+
             })).Start(); //The thread gathers, and now my watch begins. It shall not end until ^C or [Enter], I shall take no breaks (except @1KHz), hold no hooks, father no WaitHandles...
             Console.WriteLine("All systems green. [ENTER] terminates the program.");
             Console.ReadLine();
